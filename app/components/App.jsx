423c646d967c6cefadd11340170876102bfc52c2
Mario <marioperez166@gmail.com>
2019-12-06 11:49:39 +0100

    Updates

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -6,13 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {GLOBAL_CONFIG} from '../config/config.js';
 import * as I18n from '../vendors/I18n.js';
 import * as SAMPLES from '../config/samples.js';
-import {cards} from './../assets/cards.js'
+import * as GAME from './../assets/cards.js'
 
 import SCORM from './SCORM.jsx';
 import Header from './Header.jsx';
 import FinishScreen from './FinishScreen.jsx';
 import Quiz from './Quiz.jsx';
-import Card from './Card.jsx';
 import Game from './Game.jsx';
 //import PlayerBar from './PlayerBar.jsx';
 //import Shop from './Shop.jsx';
@@ -29,6 +28,7 @@ export class App extends React.Component {
     let game = "";
 
 
+
     if((this.props.tracking.finished !== true) || (GLOBAL_CONFIG.finish_screen === false)){
       appHeader = (
         <Header user_profile={this.props.user_profile} tracking={this.props.tracking} config={GLOBAL_CONFIG} I18n={I18n}/>
@@ -43,21 +43,17 @@ export class App extends React.Component {
         <FinishScreen dispatch={this.props.dispatch} user_profile={this.props.user_profile} tracking={this.props.tracking} quiz={SAMPLES.quiz_example} config={GLOBAL_CONFIG} I18n={I18n}/>
       );
     }
-    if(cards){
-      game = (<Game dispatch={this.props.dispatch} user_profile={this.props.user_profile} tracking={this.props.tracking} quiz={SAMPLES.quiz_example} config={GLOBAL_CONFIG} I18n={I18n}/>)
-    appCards=(
-      cards.map((card,i) =>
-        <span className="column1" key={i}><Card key={i} number={card.number} name={card.name} power={card.power} image={card.image} powerinfo={card.powerinfo} /></span>
-      )
-    )
+
+    if(GAME){
+       game = (<Game dispatch={this.props.dispatch} user_profile={this.props.user_profile} tracking={this.props.tracking} game={GAME.cards} config={GLOBAL_CONFIG} I18n={I18n}/>)
   }
     return (
       <div>
       <div id="container">
         <SCORM dispatch={this.props.dispatch} tracking={this.props.tracking} config={GLOBAL_CONFIG}/>
         {appHeader}
-        {game}
         {appContent}
+        {game}
         <div className="row1">
         {appCards}
         </div>
--- app/components/Game.jsx
+++ app/components/Game.jsx
@@ -1,37 +1,23 @@
 import React from 'react';
+import * as Utils from '../vendors/Utils.js';
+//import {addObjectives, resetObjectives, finishApp} from './../reducers/actions';
 
 
 //Equivale al Quiz
 export default class Game extends React.Component {
-  constructor(props){
-    super(props);
-  }
   componentDidMount(){
     // Create objectives (One per round in the game)
     let objectives = [];
-    let nQuestions = this.props.rounds.length; //Or something similar, to be determined
+    let nQuestions = this.props.game.rounds.length; //Or something similar, to be determined
     for(let i = 0; i < nQuestions; i++){
       objectives.push(new Utils.Objective({id:("Round" + (i + 1)), progress_measure:(1 / nQuestions), score:(1 / nQuestions)}));
     }
-    this.props.dispatch(addObjectives(objectives));
-  }
-  onNexRound(){
-    let isLastRound = (this.props.currentRound=== this.props.game.length); //To add these props to the Redux State
-    if(isLastRound === false){
-      this.props.dispatch(nextQuestion()); //Something similar, to be determined
-    } else {
-      this.props.dispatch(finishApp(true));
-    }
-  }
-  onResetGame(){
-    this.props.dispatch(resetGame()); //Something similar, to be determined
-    this.props.dispatch(resetObjectives());
+  //  this.props.dispatch(addObjectives(objectives));
   }
   render(){
-    let onNextRound = this.onNextRound.bind(this); //Pending, maybe not needed
-    let onResetGame = this.onResetQuiz.bind(this);
     return (
       <div>
+      Hola
       </div>
     );
   }
